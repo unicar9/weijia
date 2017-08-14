861cb138d27b0c1bd915cc009718112f578efdd5
Weijia Li <unicar9@gmail.com>
2017-08-14 21:18:41 +1000

    menu fixed

--- js/main.js
+++ js/main.js
@@ -7,33 +7,6 @@ var colors = ['#EF798A', '#613F75', '#83BCFF', '#ECCBD9'];
 
 $(document).ready(function() {
 
-  // document.addEventListener('DOMContentLoaded', function () {
-  //
-  //   // Get all "navbar-burger" elements
-  //   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  //
-  //   // Check if there are any nav burgers
-  //   if ($navbarBurgers.length > 0) {
-  //
-  //     // Add a click event on each of them
-  //     $navbarBurgers.forEach(function ($el) {
-  //       $el.addEventListener('click', function () {
-  //
-  //         // Get the target from the "data-target" attribute
-  //         var target = $el.dataset.target;
-  //         var $target = document.getElementById(target);
-  //
-  //         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-  //         $el.classList.toggle('is-active');
-  //         $target.classList.toggle('is-active');
-  //
-  //       });
-  //     });
-  //   }
-  //
-  // });
-
-
   $('.js-tilt').tilt({
     scale: 1.1
   });
